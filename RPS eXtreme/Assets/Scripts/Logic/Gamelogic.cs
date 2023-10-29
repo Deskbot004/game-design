c72f3c7740b7cbe97704f8a90853fbe12304f142
Marvin Deichsel <marvin.deichsel@gmx.de>
2023-10-29 15:48:49 +0100

    Maybe functional Symbol System

--- RPS eXtreme/Assets/Scripts/Logic/Gamelogic.cs
+++ RPS eXtreme/Assets/Scripts/Logic/Gamelogic.cs
@@ -12,6 +12,15 @@ public class Gamelogic : MonoBehaviour
     // [Non serialized]
     public Table table;
 
+    // Determines the Win of a symbol on attack
+    private int[,] winMatrix = { { -1, 1, 0, 1, 0 }, 
+                                 { 0, -1, 1, 1, 0 }, 
+                                 { 1, 0, -1, 0, 1 }, 
+                                 { 0, 0, 1, -1, 1 }, 
+                                 { 1, 1, 0, 0, -1 } };
+    // TODO WIP translator of Symbol to Entry number 
+    public Dictionary<string, int> symbolToEntry = new Dictionary<string, int>();
+
     /* Initialises the Gamelogic for the current game
      * 
      * Input: Table from which the game started.
@@ -20,6 +29,12 @@ public class Gamelogic : MonoBehaviour
     public void init(Table table)
     {
         // rework?
+        symbolToEntry.Add("Scissors", 0);
+        symbolToEntry.Add("Stone", 1);
+        symbolToEntry.Add("Paper", 2);
+        symbolToEntry.Add("Lizard", 3);
+        symbolToEntry.Add("Spock", 4);
+
         Debug.Log("Game started");
         this.table = table;
         currentLifepoints.Add("user", lifepointMax);
@@ -67,7 +82,7 @@ public class Gamelogic : MonoBehaviour
                 {
                     slotUser.TurnCards();
                     slotEnemy.TurnCards();
-                    string winner = EvaluateCards(slotUser.GetCard(), slotEnemy.GetCard());
+                    string winner = EvaluateCards(slotUser.GetCards(), slotEnemy.GetCards());
                     table.ResolveSlot(slotUser.GetPosition(), winner);
                 }
             }
@@ -94,34 +109,59 @@ public class Gamelogic : MonoBehaviour
     * Input: cardUser the card(s) played by the user, cardEnemy the card(s) played by the enemy
     * Output: string the Winner of the round or none
     */
-    private string EvaluateCards(Card cardUser, Card cardEnemy)
+    private string EvaluateCards(Card[] cardsUser, Card[] cardsEnemy)
     {   
         // if no card was played on either slot
-        if (cardUser == null && cardEnemy == null)
+        if (cardsUser == null && cardsEnemy == null)
         {
             currentLifepoints["user"] -= 1;
             return "none";
-        } else if (cardEnemy == null)
+        } else if (cardsEnemy == null)
         {
             currentLifepoints["enemy"] -= 1;
             return "user";
-        } else if (cardUser == null)
+        } else if (cardsUser == null)
         {
             currentLifepoints["enemy"] -= 1;
             return "user";
         }
 
-        if (cardUser.GetValue() > cardEnemy.GetValue())
+        int symbolToEntryUser = 0;
+        int symbolToEntryEnemy = 0;
+
+        foreach (Card card in cardsUser)
+        {
+            if (card.isBasic())
+            {
+                symbolToEntryUser = symbolToEntry[card.GetSymbol()];
+            }
+        }
+
+        foreach (Card card in cardsEnemy)
+        {
+            if (card.isBasic())
+            {
+                symbolToEntryEnemy = symbolToEntry[card.GetSymbol()];
+            }
+        }
+
+        int attack = winMatrix[symbolToEntryUser,symbolToEntryEnemy];
+        if (attack == -1)
+        {
+            return "none";
+        } else if (attack == 1)
         {
             currentLifepoints["enemy"] -= 1;
             return "user";
-        } else if(cardUser.GetValue() < cardEnemy.GetValue())
+        } else if (attack == 0)
         {
             currentLifepoints["user"] -= 1;
             return "enemy";
+        } else
+        {
+            Debug.Log("Error: unknown winstate");
+            return "none";
         }
-
-        return "none";
     }
 
     /* Function to notify the table if the game ended.
