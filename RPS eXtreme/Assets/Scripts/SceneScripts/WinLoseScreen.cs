a70db8251fb40a69674eece80431a1037728ff60
Loarana <julia.kriv@gmail.com>
2024-01-21 09:48:25 +0100

    Added WinScreen to Main Game

--- RPS eXtreme/Assets/Scripts/SceneScripts/WinLoseScreen.cs
+++ RPS eXtreme/Assets/Scripts/SceneScripts/WinLoseScreen.cs
@@ -11,13 +11,17 @@ public class WinLoseScreen : MonoBehaviour
     public void showWinner(string winner)
     {
         bool playerWon = winner == "user";
-        if(!playerWon) winText.text = "You Lost...";
+        if(!playerWon) 
+        {
+            winText.text = "You Lost...";
+        }
         gameObject.SetActive(true);
+        GetComponent<Animator>().SetBool("playerWon", playerWon);
     }
 
     public void PlayAgain()
     {
-        SceneManager.LoadScene("Main Game Julia");
+        SceneManager.LoadScene("Main Game");
     }
 
     public void MainMenu()
