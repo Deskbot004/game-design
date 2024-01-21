bcea196cf5b97265df4066a70b6b3f9dd34e7349
Marvin Deichsel <marvin.deichsel@gmx.de>
2024-01-21 20:39:32 +0100

    cleanup C...cs

--- RPS eXtreme/Assets/Scripts/Cards/Card.cs
+++ RPS eXtreme/Assets/Scripts/Cards/Card.cs
@@ -8,8 +8,10 @@ using UnityEditor;
 public class Card : MonoBehaviour
 {
     public string symbol;
-    public int slotType = -1;   //For Normal:  0: has no slots, 1: has a top slot, 2: has a bottom slot; 3: has both
-                                //For Support: 0: fits in top slot, 1: fits in bottom slot
+    
+    // For Normal:  0: has no slots, 1: has a top slot, 2: has a bottom slot; 3: has both
+    // For Support: 0: fits in top slot, 1: fits in bottom slot
+    public int slotType = -1;
 
     private string[] viableStrings = { "scissors", "rock", "paper", "lizard", "spock", "support" };
     
@@ -19,7 +21,8 @@ public class Card : MonoBehaviour
 
     public AudioClip playWhenMoved;
 
-    protected int status = -1; //-1: outside of game, 0: in a pile, 1: in hand 2: in slot // TODO sollte in verschiedenen funktionen angepass werden
+    // -1: outside of game, 0: in a pile, 1: in hand 2: in slot
+    protected int status = -1;
 
     // Animation stuff
     private Vector3 targetPosition; // WorldPosition
@@ -53,85 +56,15 @@ public class Card : MonoBehaviour
     }
 
     /*
-     * Checks, if the card is a SupportCard or a NormalCard. false = Support, true = Normal.
-     */
+    * Checks, if the card is a SupportCard or a NormalCard. false = Support, true = Normal.
+    */
 
     public virtual bool IsBasic()
     {
         return false;
     }
-
-    // ---------- Getter & Setter ------------------------------------------------------------------------------
-
-    public string GetSymbol(){return this.symbol;}
-
-    public int SetSymbol(string newSymbol)
-    {
-        if(Array.Exists(viableStrings,element => element == newSymbol))
-        {
-            this.symbol = newSymbol;
-            return 0;
-        }
-        else
-        {
-            //Debug.Log("The given symbol is not a viable symbol");
-            return -1;
-        }
-    }
-
-    public int GetSlotType(){return this.slotType;}
-
-    public virtual int SetSlotType(int type)
-    {
-        this.slotType = type;
-        return 0;
-    }
-
-    public CardSprites GetCardSprites() { return this.cardSprites; }
-
-    public virtual void SetSprite()
-    {
-        cardSprites = transform.GetComponent<CardSprites>();
-        if (GetCardSprites().colors.ContainsKey(GetSymbol()))
-            transform.Find("Card Sprites/Background").GetComponent<SpriteRenderer>().color = GetCardSprites().colors[GetSymbol()]; // Set color
-        if (GetCardSprites().colors.ContainsKey(GetSymbol() + " window"))
-            transform.Find("Card Sprites/Picture Background").GetComponent<SpriteRenderer>().color = GetCardSprites().colors[GetSymbol() + " window"]; // Set color
-
-        /*
-        if(deck != null && !deck.GetTablePlayer().isPlayer)
-        {
-            transform.Find("Card Sprites/Cardback").gameObject.SetActive(true);
-            transform.Find("Card Sprites/Upper Effect").gameObject.SetActive(false);
-            transform.Find("Card Sprites/Lower Effect").gameObject.SetActive(false);
-            return;
-        }
-        */
-    }
-
-    public void flipCard()
-    {
-        transform.Find("Card Sprites/Cardback").gameObject.SetActive(!transform.Find("Card Sprites/Cardback").gameObject.activeSelf);
-    }
-
-    public void SetStatus(int status) {this.status = status;}
-
-    public int GetStatus() {return this.status;}
-    public Deck GetDeck() {return deck;}
-
-    public void SetWorldTargetPosition(Vector3 position)
-    {
-        targetPosition = position;
-    }
-    public void SetTargetRotation(Vector3 rotation)
-    {
-        targetRotation = rotation;
-    }
-
     public IEnumerator MoveToTarget(float moveTime, bool activeOnArrival = true)
     {
-        //bool dragEnabled = GetComponent<Draggable>().enabled;
-        //GetComponent<Draggable>().enabled = false;
-
         Vector3 startingPos  = transform.position;
         Vector3 finalPos = targetPosition;
         Quaternion startingRotation = transform.localRotation;
@@ -154,7 +87,61 @@ public class Card : MonoBehaviour
                 if (audio.clip == playWhenMoved) audio.Play();
             }
         }
-        //GetComponent<Draggable>().enabled = dragEnabled;
+    }
+
+    public void flipCard()
+    {
+        transform.Find("Card Sprites/Cardback").gameObject.SetActive(!transform.Find("Card Sprites/Cardback").gameObject.activeSelf);
+    }
+
+    // ---------- Getter & Setter ------------------------------------------------------------------------------
+
+    public string GetSymbol(){return this.symbol;}
+
+    public int SetSymbol(string newSymbol)
+    {
+        if(Array.Exists(viableStrings,element => element == newSymbol))
+        {
+            this.symbol = newSymbol;
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public int GetSlotType(){return this.slotType;}
+
+    public virtual int SetSlotType(int type)
+    {
+        this.slotType = type;
+        return 0;
+    }
+
+    public CardSprites GetCardSprites() { return this.cardSprites; }
+
+    public virtual void SetSprite()
+    {
+        cardSprites = transform.GetComponent<CardSprites>();
+        if (GetCardSprites().colors.ContainsKey(GetSymbol()))
+            transform.Find("Card Sprites/Background").GetComponent<SpriteRenderer>().color = GetCardSprites().colors[GetSymbol()]; // Set color
+        if (GetCardSprites().colors.ContainsKey(GetSymbol() + " window"))
+            transform.Find("Card Sprites/Picture Background").GetComponent<SpriteRenderer>().color = GetCardSprites().colors[GetSymbol() + " window"]; // Set color
+    }
+
+    public void SetStatus(int status) {this.status = status;}
+
+    public int GetStatus() {return this.status;}
+    public Deck GetDeck() {return deck;}
+
+    public void SetWorldTargetPosition(Vector3 position)
+    {
+        targetPosition = position;
+    }
+    public void SetTargetRotation(Vector3 rotation)
+    {
+        targetRotation = rotation;
     }
 
     public virtual List<(Action<Gamelogic, string, object>, object)> GetFunctionsAR() { return null; }
@@ -174,8 +161,6 @@ public class Card : MonoBehaviour
         if (this == null) return;
         else if (gameObject.scene.name == null) return;
         SetSprite();
-        //if (SetSymbol(symbol) < 0) { return; }
-        //else { SetSprite(); }
     }
 #endif
 }
